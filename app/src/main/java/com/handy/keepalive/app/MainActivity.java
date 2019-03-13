7dd6ce6250cdf44c8c7ccf46663d19a148f92ad6
liujie <liujie045.china@outlook.com>
2019-03-13 14:24:07 +0800

    Initial commit

---
+++ app/src/main/java/com/handy/keepalive/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.handy.keepalive.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.handy.keepalive.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
