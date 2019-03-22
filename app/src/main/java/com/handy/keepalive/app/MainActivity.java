43513511a87740e88eb6c2af0436a6232e36f8f8
liujie <liujie045.china@outlook.com>
2019-03-22 17:37:41 +0800

    Handy Commit

--- app/src/main/java/com/handy/keepalive/app/MainActivity.java
+++ app/src/main/java/com/handy/keepalive/app/MainActivity.java
@@ -1,15 +1,87 @@
 package com.handy.keepalive.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.handy.keepalive.app.R;
+import com.handy.keepalive.BaseServiceConnection;
+import com.handy.keepalive.utils.ServiceUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected TextView tvContent;
+    protected Button btnDefaultStart;
+    protected Button btnDefaultStop;
+    protected Button btnAliveStart;
+    protected Button btnAliveStop;
+
+    private BaseServiceConnection baseServiceConnection = new BaseServiceConnection() {
+        @Override
+        public void onConnected(ComponentName name, IBinder service) {
+            super.onConnected(name, service);
+            TimeLockService.MyBinder binder = (TimeLockService.MyBinder) service;
+            binder.setResultListener(new TimeLockService.ResultListener() {
+                @Override
+                public void onResult(final String string) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvContent.setText(string);
+                        }
+                    });
+                }
+            });
+        }
+
+        @Override
+        public void onDisconnected(ComponentName name) {
+            super.onDisconnected(name);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvContent.setText("Hello World");
+                }
+            });
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvContent = findViewById(R.id.tv_content);
+        btnDefaultStart = findViewById(R.id.btn_default_start);
+        btnDefaultStop = findViewById(R.id.btn_default_stop);
+        btnAliveStart = findViewById(R.id.btn_alive_start);
+        btnAliveStop = findViewById(R.id.btn_alive_stop);
+
+        btnDefaultStart.setOnClickListener(this);
+        btnDefaultStop.setOnClickListener(this);
+        btnAliveStart.setOnClickListener(this);
+        btnAliveStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_default_start:
+                ServiceUtil.startService(MainActivity.this, TimeLockService.class);
+                break;
+            case R.id.btn_default_stop:
+                ServiceUtil.stopService(MainActivity.this, TimeLockService.class);
+                break;
+            case R.id.btn_alive_start:
+                ServiceUtil.bindService(MainActivity.this, TimeLockService.class, baseServiceConnection);
+                break;
+            case R.id.btn_alive_stop:
+                ServiceUtil.unbindService(MainActivity.this, TimeLockService.class, baseServiceConnection);
+                break;
+            default:
+        }
     }
 }
