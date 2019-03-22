48952efb417c629d5e926bf8312268e4e5a645ce
liujie <liujie045.china@outlook.com>
2019-03-22 18:01:56 +0800

    Handy Commit

--- library/src/main/java/com/handy/keepalive/utils/ServiceUtil.java
+++ library/src/main/java/com/handy/keepalive/utils/ServiceUtil.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.handy.keepalive.BaseServiceConnection;
-import com.handy.keepalive.config.Config;
 
 import java.util.Objects;
 
@@ -45,7 +44,7 @@ public class ServiceUtil {
 
     public static void stopService(Context context, Class<? extends Service> serviceClass) {
         try {
-            finishService(context, serviceClass);
+            context.stopService(new Intent(context, serviceClass));
         } catch (Exception ignored) {
 
         }
@@ -54,30 +53,14 @@ public class ServiceUtil {
     public static void startAndBindService(@NonNull final Context context, @NonNull final Class<? extends Service> serviceClass, @NonNull BaseServiceConnection connection) {
         if (!connection.isConnected) {
             startService(context, serviceClass);
-            context.bindService(new Intent(context, serviceClass), connection, Context.BIND_AUTO_CREATE);
+            bindService(context, serviceClass, connection);
         }
     }
 
     public static void stopAndUnbindService(@NonNull final Context context, @NonNull final Class<? extends Service> serviceClass, @NonNull BaseServiceConnection connection) {
         if (connection.isConnected) {
-            context.unbindService(connection);
-            connection.onServiceDisconnected(new ComponentName(Objects.requireNonNull(serviceClass.getPackage()).getName(), serviceClass.getName()));
-            finishService(context, serviceClass);
-        }
-    }
-
-    /**
-     * 当前哪个进程使用的时候 就用其上下文发送广播
-     */
-    public static void finishService(Context context, @NonNull final Class<? extends Service> serviceClass) {
-        if (context != null) {
-            context.sendBroadcast(new Intent(serviceClass.getName()));
-        }
-    }
-
-    public static void finishAllServices(Context context) {
-        if (context != null) {
-            context.sendBroadcast(new Intent(Config.STOP_BROADCASTRECEIVER_IDENTIFIER));
+            unbindService(context, serviceClass, connection);
+            stopService(context, serviceClass);
         }
     }
 }
