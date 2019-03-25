1aaf24e8371c79b944d18dcaa918b462f3d43c18
liujie <liujie045.china@outlook.com>
2019-03-25 14:49:13 +0800

    Handy Commit

--- library/src/main/java/com/handy/keepalive/service/PlayMusicService.java
+++ library/src/main/java/com/handy/keepalive/service/PlayMusicService.java
@@ -50,7 +50,7 @@ public class PlayMusicService extends Service {
                 }
                 return null;
             }
-        };
+        }.execute();
         return START_STICKY;
     }
 
